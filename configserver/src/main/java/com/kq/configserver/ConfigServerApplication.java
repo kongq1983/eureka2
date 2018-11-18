71e5e497855d174ff959cf69b0dc76bc32b19f2f
king <kongq1983@qq.com>
2018-11-18 20:49:30 +0800

    modify package

--- configserver/src/main/java/com/kq/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/kq/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package java.com.kq.configserver;
+package com.kq.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
