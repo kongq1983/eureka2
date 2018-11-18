e08e9668558d99096242ef1a19a39a3f4b726a92
king <kongq1983@qq.com>
2018-11-18 20:31:05 +0800

    eureka2 project first submit

---
+++ configserver/src/main/java/com/kq/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package java.com.kq.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * http://www.springboottutorial.com/spring-boot-and-spring-jdbc-with-h2
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
